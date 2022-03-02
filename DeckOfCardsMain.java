740d63cfb67421fd0cf622544be13aaeae404c79
AkshataHasabi <akshuh818@gmail.com>
2022-03-02 10:37:07 +0530

    ADDED:created object to implement

--- DeckOfCardsMain.java
+++ DeckOfCardsMain.java
@@ -3,6 +3,9 @@ package com.bridgelab;
 public class DeckOfCardsMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to day 11&12 programms");
+		DeckOfCardServices deckOfCardService = new DeckOfCardsImpl();
+		deckOfCardService.start();
+		deckOfCardService.dequeuePlayers();
 	}
 
 }
