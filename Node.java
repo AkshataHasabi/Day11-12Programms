c1b3628902143749542b237bc89b0be2d447ca35
AkshataHasabi <akshuh818@gmail.com>
2022-03-02 10:43:16 +0530

    ADDED:Node details

---
+++ Node.java
@@ -0,0 +1,13 @@
+package com.bridgelab;
+
+public class Node<T> {
+	T data; // data to be stored
+	Node<T> next; // reference to the next node
+
+	Node(T data) {
+		this.data = data;
+		next = null;
+	}
+}
+
+
